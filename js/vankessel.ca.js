4bd182936d2fa92cfa465b7c0493de8cddde85e0
Kevin Van Kessel <10148659+vankessel@users.noreply.github.com>
2015-11-05 16:06:33 -0800

    Set seed to change every day and page

--- js/vankessel.ca.js
+++ js/vankessel.ca.js
@@ -22,7 +22,7 @@ function generateBackground() {
     cell_size: 256,
     variance: 0.75,
     stroke_width: 1.4,
-    seed: document.title
+    seed: (new Date).toDateString() + document.title
   }).svg();
   
   svg.setAttribute("xmlns", "http://www.w3.org/2000/svg");
