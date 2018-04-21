960d840109ff1b22e1441a2ea20a2a777d237e19
Kevin Van Kessel <10148659+vankessel@users.noreply.github.com>
2018-04-20 19:55:30 -0700

    Update gtag.js

--- assets/js/gtag.js
+++ assets/js/gtag.js
@@ -1,4 +1,4 @@
-<!-- Global site tag (gtag.js) - Google Analytics -->
+//<!-- Global site tag (gtag.js) - Google Analytics -->
 window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
