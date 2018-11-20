e44658f678d8a128acffc6c83e337ae7707c9bbb
Kevin Van Kessel <10148659+vankessel@users.noreply.github.com>
2018-11-19 18:55:13 -0800

    Removed old blur and fall cleaning

--- assets/js/vankessel.js
+++ assets/js/vankessel.js
@@ -1,65 +1,35 @@
 $(document).ready(function() {
-  generateBackground();
+  main();
 });
 
 MathJax.Hub.Register.StartupHook("End", function() {
-  main();
+  generateBackground();
 });
 
 function main() {
 
-  resetVisuals();
+  generateBackground();
 
-  //Perform actions and allow event to fire again after timeout so
-  //these expensive functions don't get called many times during a resize
-  var resizable = true;
   $(window).resize(function() {
-    if(resizable) {
-      resizable = false;
-      resetVisuals();
-      setTimeout(function() {
-        resizable = true;
-      }, 500)
-    }
+    generateBackground();
   });
 
-  //Automatically set lightboxes for images in #post
+  // Set lightboxes for images in #post
   $("#post img").each(function() {
     $(this).attr("data-featherlight", $(this).attr("src"));
   });
 }
 
-function resetVisuals() {
-  generateBackground();
-  setTimeout(function() {
-    applyBlur();
-  }, 1000);
-}
-
-function applyBlur() {
-
-  $('.blur').blurjs({
-    source: 'html',
-    radius: 16,
-    overlay: 'rgba(255,255,255,0.20)'
-  });
-}
-
 function generateBackground(seed) {
 
-  let w = $(window).width();
-  let h = $(document).height();
-
   let pattern = Trianglify({
-    width: w,
-    height: h,
+    width: $(document).width(),
+    height: $(document).height(),
     cell_size: 192,
     variance: 0.75,
     stroke_width: 1.4,
     seed: seed || (new Date).toDateString() + document.title
   });
-
-  // document.body.appendChild(pattern.canvas());
 
   let svg = pattern.svg();
   svg.setAttribute("xmlns", "http://www.w3.org/2000/svg");
